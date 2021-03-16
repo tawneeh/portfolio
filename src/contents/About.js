928e9c7092b40471cb52edbd4be89878074e532e
Tawnee Harris <tawneeh@icloud.com>
2021-03-16 12:23:05 -0700

    add About me

---
+++ src/contents/About.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class About extends Component {
+  render() {
+    return (
+      <div className="condiv">
+        <h1 className="subtopic">About Me</h1>
+          <h4>Hello,</h4>
+          <h1>I'm Tawnee Harris</h1>
+          <h3>Junior Web <u>Developer</u> | UI/UX <u>Designer</u></h3>
+          <br />
+          <p>My journey to the tech industry has had a few twists and turns. I never thought that I would be a developer but I have successfully completed the Epicodus twenty week C#/React bootcamp!</p>
+          <br />
+          <p><em>Certificate in Web and Mobile Development</em></p>
+      </div>
+    )
+  }
+}
+
+export default About; // really spruce this up and add cool shit
