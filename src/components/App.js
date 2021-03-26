38614e5fe4d6f23a58ee0318d0bd6f5866f2befc
Tawnee Harris <tawneeh@icloud.com>
2021-03-26 15:00:53 -0700

    update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,35 @@
-import Header from './Header';
+import Sidebar from './Sidebar';
+import Home from './../contents/Home';
+import About from './../contents/About';
+import Education from './../contents/Education';
+import Skills from './../contents/Skills';
+import Contact from './../contents/Contact';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './../App.css';
 
 function App() {
   return (
     <>
-      <Header />
+      <Router>
+        <div className="App">
+          <Sidebar />
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route exact path="/about">
+              <About />
+            </Route>
+            <Route path="/education">
+              <Education />
+            </Route>
+            <Route path="/skills">
+              <Skills />
+            </Route>
+            <Route path="/contact">
+              <Contact />
+            </Route>
+        </div>
+      </Router>
     </>
   );
 }
