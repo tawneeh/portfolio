83d89c558de27de3fde63c2e43b5df59533ea9b4
Tawnee Harris <tawneeh@icloud.com>
2021-03-28 13:32:14 -0700

    update Skills

--- src/contents/Skills.js
+++ src/contents/Skills.js
@@ -1,24 +1,20 @@
 import React, { Component } from 'react';
 
 class Skills extends Component {
-  constructor(props) {
-    super(props);
-      this.state={
-        'myskills':['HTML','CSS','JS','REACT JS','FIREBASE']
-      };
-    }
   render() {
     return (
       <div className="condiv skills">
         <h1 className="subtopic">My Skills</h1>
           <ul>
-            {this.state.myskills.map((value)=>{
-              return <li>{value}</li>
-            })}
+            <li>HTML</li>
+            <li>CSS</li>
+            <li>JS</li>
+            <li>React JS</li>
+            <li>FIREBASE</li>
           </ul>
       </div>
     )
   }
 }
 
-export default Skills; // its just a regular old bulleted list rn!
+export default Skills;
