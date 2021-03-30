2ba207e94afaa9b96fd8772974b64ee7dbcf0b1d
Tawnee Harris <tawneeh@icloud.com>
2021-03-30 11:18:56 -0700

    remove Social component from Sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,10 +1,5 @@
 import React, { Component } from 'react';
 import Navitem from './Navitem';
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faGithub,
-  faLinkedin
-} from "@fortawesome/free-brands-svg-icons";
 
 class Sidebar extends Component {
   constructor(props)
@@ -30,19 +25,10 @@ class Sidebar extends Component {
         <nav>
           <ul>
             <Navitem item="Home" tolink="/"  activec={this.activeitem}></Navitem>
-            <Navitem item="About" tolink="/about"  activec={this.activeitem}></Navitem>
+            <Navitem item="About Me" tolink="/about"  activec={this.activeitem}></Navitem>
             <Navitem item="Education" tolink="/education"  activec={this.activeitem}></Navitem>
             <Navitem item="Skills" tolink="/skills"  activec={this.activeitem}></Navitem>
-            <Navitem item="Contact" tolink="/contact"  activec={this.activeitem}></Navitem>
           </ul>
-            <a href="https://www.linkedin.com/in/tawneeh/"
-              className="linkedin social">
-              <FontAwesomeIcon icon={faLinkedin} size="2x" />
-            </a>
-            <a href="https://github.com/tawneeh/"
-              className="github social">
-              <FontAwesomeIcon icon={faGithub} size="2x" />
-            </a>
         </nav>
       )
     }
