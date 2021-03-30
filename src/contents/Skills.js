349cb0b0d16f2f82d51f3cb8f79920f7960ba197
Tawnee Harris <tawneeh@icloud.com>
2021-03-29 17:00:44 -0700

    add animation to Skills list

--- src/contents/Skills.js
+++ src/contents/Skills.js
@@ -1,20 +1,38 @@
-import React, { Component } from 'react';
+import React, { useState } from "react";
+import { useTrail, animated as a } from "react-spring";
+import "./spring.css";
 
-class Skills extends Component {
-  render() {
-    return (
-      <div className="condiv skills">
-        <h1 className="subtopic">My Skills</h1>
-          <ul>
-            <li>HTML</li>
-            <li>CSS</li>
-            <li>JS</li>
-            <li>React JS</li>
-            <li>FIREBASE</li>
-          </ul>
+const skills = ["HTML", "CSS", "JS", "REACT JS", "FIREBASE"];
+const config = { mass: 5, tension: 2000, friction: 200 };
+
+function Skills() {
+  const [toggle, set] = useState(true);
+  const trail = useTrail(skills.length, {
+    config,
+    opacity: toggle ? 1 : 0,
+    x: toggle ? 0 : 20,
+    height: toggle ? 80 : 0,
+    from: { opacity: 0, x: 20, height: 0 }
+  });
+
+  return (
+    <div className="trails-main" onClick={() => set(state => !state)}>
+      <div>
+        {trail.map(({ x, height, ...rest }, index) => (
+          <a.div
+            key={skills[index]}
+            className="trails-text"
+            style={{
+              ...rest,
+              transform: x.interpolate(x => `translate3d(0,${x}px,0)`)
+            }}
+          >
+            <a.div style={{ height }}>{skills[index]}</a.div>
+          </a.div>
+        ))}
       </div>
-    )
-  }
+    </div>
+  );
 }
 
 export default Skills;
