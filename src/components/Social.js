4d66e07bb9eb25eee100c9e546525ed096209e76
Tawnee Harris <tawneeh@icloud.com>
2021-04-02 15:45:13 -0700

    add Epicodus link button

--- src/components/Social.js
+++ src/components/Social.js
@@ -2,8 +2,9 @@ import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faGithub,
-  faLinkedin
+  faLinkedin,
 } from "@fortawesome/free-brands-svg-icons";
+import { faCoffee } from '@fortawesome/free-solid-svg-icons'
 
 
 export default function SocialFollow() {
@@ -16,6 +17,10 @@ export default function SocialFollow() {
       <a href="https://github.com/tawneeh/"
         className="github social">
         <FontAwesomeIcon icon={faGithub} size="2x" />
+      </a>
+      <a href="https://www.epicodus.com/"
+        className="epicodus social">
+        <FontAwesomeIcon icon={faCoffee} size="2x" />
       </a>
     </div>
   );
