7c16efce6b799108c194ea4876d01af243963bf5
Tawnee Harris <tawneeh@icloud.com>
2021-04-30 16:16:12 -0700

    add inline styling to Links

--- src/components/Navitem.js
+++ src/components/Navitem.js
@@ -5,7 +5,7 @@ class Navitem extends Component {
   render() {
     return (
       <li id={this.props.item}>
-        <Link to={this.props.tolink} onClick={this.props.activec.bind(this, this.props.item)}>{this.props.item}</Link>
+        <Link style={{ color: "#6dd0db", textDecoration: 'none', fontFamily: 'Impact' }} to={this.props.tolink} onClick={this.props.activec.bind(this, this.props.item)}>{this.props.item}</Link>
       </li>
     )
   }
