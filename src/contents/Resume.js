af4d64e46713acd02361f58744206ba6d113c8e5
Tawnee Harris <tawneeh@icloud.com>
2021-04-30 16:12:50 -0700

    add Resume component

--- src/contents/Resume.js
+++ src/contents/Resume.js
@@ -1,17 +1,11 @@
 import React, { Component } from 'react';
+import resume from '../img/resume.png';
 
 class Resume extends Component {
   render() {
     return (
       <>
-        <div style="position: relative; width: 100%; height: 0; padding-top: 129.4118%;
-    padding-bottom: 48px; box-shadow: 0 2px 8px 0 rgba(63,69,81,0.16); margin-top: 1.6em; margin-bottom: 0.9em; overflow: hidden;
-    border-radius: 8px; will-change: transform;">
-          <iframe style="position: absolute; width: 100%; height: 100%; top: 0; left: 0; border: none; padding: 0;margin: 0;"
-        src="https:&#x2F;&#x2F;www.canva.com&#x2F;design&#x2F;DAETOZPZp8A&#x2F;view?embed">
-          </iframe>
-        </div>
-        <a href="https:&#x2F;&#x2F;www.canva.com&#x2F;design&#x2F;DAETOZPZp8A&#x2F;view?utm_content=DAETOZPZp8A&amp;utm_campaign=designshare&amp;utm_medium=embeds&amp;utm_source=link" target="_blank" rel="noopener">Original</a>
+        <img src={ resume } className="resumeImg" alt="My Resume"></img>
       </>
     )
   }
@@ -19,4 +13,4 @@ class Resume extends Component {
 
 export default Resume;
 
-// add Resume to Sidebar.js once updated to JSX format
+// work on styling this
