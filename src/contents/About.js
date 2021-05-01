3d78df86050dd1729243e6aa6cae3e4362973d66
Tawnee Harris <tawneeh@icloud.com>
2021-04-30 18:55:18 -0700

    update About

--- src/contents/About.js
+++ src/contents/About.js
@@ -4,16 +4,16 @@ class About extends Component {
   render() {
     return (
       <div className="condiv">
-          <h4>Hello!</h4>
-          <h1>I'm Tawnee Harris</h1>
+          <h1>About Tawnee</h1>
           <h3 style={{ color: '#6dd0db' }}>Junior Software Developer || Certificate in Web and Mobile Development</h3>
           <br />
-          <p>I custom built this site using React.js to showcase my coding abilities. Enjoy perusing my site to your heart's content! If you would like to see more of my work, please check out my GitHub.</p>
-          <p>Head over to LinkedIn and connect with me!</p>
+          <p>Tawnee Harris is a Junior Developer living in the Seattle area. SHe is a recent Epicodus graduate interested in data engineering and is extremely eager to continue learning!</p>
+          <br />
+          <p>Tawnee custom built this website using React.js to showcase her coding abilities. Enjoy perusing the site to your heart's content! If you would like to see more of Tawnee's work, please check out her GitHub. </p>
           <a href="https://github.com/tawneeh">GitHub</a>
           <br />
+          <p>Head over to LinkedIn and connect with me!</p>
           <a href="https://www.linkedin.com/in/tawneeh/">LinkedIn</a>
-          <br />
       </div>
     )
   }
