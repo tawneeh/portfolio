40b820409bd890f85e4c0967865e0ec8d9018c14
Tawnee Harris <tawneeh@icloud.com>
2021-05-01 00:10:43 -0700

    adjust TypingEffect above image

--- src/contents/Home.js
+++ src/contents/Home.js
@@ -7,12 +7,12 @@ class Home extends Component {
     return (
       <div className="condiv home">
         <h1 id="myName"><span className="dropCap">T</span>AWNEE <span className="dropCap">H</span>ARRIS</h1>
+        <ReactTypingEffect className="typingeffect" text={['Hi! My name is Tawnee','I develop software']} speed={100} eraseDelay={700} />
+        <br />
+        <br />
         <div className="image image-colorize">
           <img src={ profilepic } alt="Tawnee Harris"></img>
         </div>
-        <br />
-        <br />
-        <ReactTypingEffect className="typingeffect" text={['Hi! My name is Tawnee','I develop software']} speed={100} eraseDelay={700} />
       </div>
     )
   }
