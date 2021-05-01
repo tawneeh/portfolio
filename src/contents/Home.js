971660c9f28b3f46a9038f88aab282af3751a46a
Tawnee Harris <tawneeh@icloud.com>
2021-04-30 23:49:36 -0700

    edit image container and class name

--- src/contents/Home.js
+++ src/contents/Home.js
@@ -7,7 +7,9 @@ class Home extends Component {
     return (
       <div className="condiv home">
         <h1 id="myName"><span className="dropCap">T</span>AWNEE <span className="dropCap">H</span>ARRIS</h1>
-        <img src={ profilepic } className="profilepic" alt="Tawnee Harris"></img>
+        <div className="image image-colorize">
+          <img src={ profilepic } alt="Tawnee Harris"></img>
+        </div>
         <br />
         <br />
         <ReactTypingEffect className="typingeffect" text={['Hi! My name is Tawnee','I develop software']} speed={100} eraseDelay={700} />
