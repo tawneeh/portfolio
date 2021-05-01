1fc52e30e92b45e0919ff06effe35592c87e791c
Tawnee Harris <tawneeh@icloud.com>
2021-05-01 13:47:15 -0700

    remove header tag

--- src/contents/Education.js
+++ src/contents/Education.js
@@ -4,9 +4,8 @@ import Card from '../components/Card';
 class Education extends Component {
   render() {
     return (
-      <div className="condiv">
-        <h1 className="subtopic">Education</h1>
-          <Card title="Web and Mobile Development" where="Epicodus School for Tech Careers" from="October 2020" to="March 2021" />
+      <div>
+          <Card title="Epicodus School for Tech Careers" where="Web and Mobile Development" from="October 2020" to="March 2021" />
       </div>
     )
   }
