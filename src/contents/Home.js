81ba39f90ce0c0ce748a18e5016077b24b43ad73
Tawnee Harris <tawneeh@icloud.com>
2021-05-01 13:47:50 -0700

    adjust Typing Effect placement

--- src/contents/Home.js
+++ src/contents/Home.js
@@ -7,11 +7,10 @@ class Home extends Component {
     return (
       <div className="condiv home">
         <h1 id="myName"><span className="dropCap">T</span>AWNEE <span className="dropCap">H</span>ARRIS</h1>
-        <ReactTypingEffect className="typingeffect" text={['Hi! My name is Tawnee','I develop software']} speed={100} eraseDelay={700} />
-        <br />
-        <br />
         <div className="image image-colorize">
           <img src={ profilepic } alt="Tawnee Harris"></img>
+        <br />
+        <ReactTypingEffect className="typingeffect" text={['Hi! My name is Tawnee','I develop software']} speed={100} eraseDelay={700} />
         </div>
       </div>
     )
