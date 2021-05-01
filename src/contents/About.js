7aa5d5f7782bad60d53cacf391b86be1d48667a0
Tawnee Harris <tawneeh@icloud.com>
2021-05-01 13:46:33 -0700

    add Card

--- src/contents/About.js
+++ src/contents/About.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Education from './Education';
 
 class About extends Component {
   render() {
@@ -6,15 +7,15 @@ class About extends Component {
       <div className="condiv">
           <h1>About Tawnee</h1>
           <h3 style={{ color: '#6dd0db' }}>Junior Software Developer || Certificate in Web and Mobile Development</h3>
-          <br />
-          <p>Tawnee Harris is a Junior Developer living in the Seattle area. She is a recent Epicodus graduate interested primarily in data engineering and is extremely eager to continue learning!</p>
-          <br />
-          <p>Tawnee custom built this website using React.js to showcase her coding abilities. Enjoy perusing the site to your heart's content! If you would like to see more of Tawnee's work, please check out her GitHub. </p>
-          <a href="https://github.com/tawneeh"><em>GitHub</em></a>
-          <br />
-          <br />
-          <p>Head over to LinkedIn and connect with Tawnee!</p>
-          <a href="https://www.linkedin.com/in/tawneeh/"><em>LinkedIn</em></a>
+              <br />
+            <p>Tawnee Harris is a Junior Developer living in the Seattle area. She is a recent Epicodus graduate interested primarily in data engineering and is extremely eager to continue learning!</p>
+            <Education />
+              <br />
+            <p>Tawnee custom built this website using React.js to showcase her coding abilities. Enjoy perusing the site to your heart's content! If you would like to see more of Tawnee's work, please check out her GitHub. </p>
+              <a href="https://github.com/tawneeh"><em>GitHub</em></a>
+                <br />
+            <p>Head over to LinkedIn and connect with Tawnee!</p>
+              <a href="https://www.linkedin.com/in/tawneeh/"><em>LinkedIn</em></a>
       </div>
     )
   }
