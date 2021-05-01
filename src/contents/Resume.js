c318086b33e1163e84957d4fd50586bda02b67c8
Tawnee Harris <tawneeh@icloud.com>
2021-05-01 13:48:28 -0700

    add mailto and condiv class

--- src/contents/Resume.js
+++ src/contents/Resume.js
@@ -4,13 +4,12 @@ import resume from '../img/resume.png';
 class Resume extends Component {
   render() {
     return (
-      <>
+      <div className="condiv">
+        <h2 style={{ textAlign: 'center' }}><em><a href="mailto:tawneeh@icloud.com">Email Tawnee</a></em></h2>
         <img src={ resume } className="resumeImg" alt="My Resume"></img>
-      </>
+      </div>
     )
   }
 }
 
 export default Resume;
-
-// work on styling this
